511660afe930f0da2f979079c48826091b3c8404
Adnan Alam Ansari <adnanalam326@gmail.com>
2021-04-22 17:44:41 -0300

    using tabular choice inline and extra fields in question admin

--- polls/admin.py
+++ polls/admin.py
@@ -3,16 +3,12 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 3
-
+    extra = 0
 
 class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
     inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
